25bba38bc8e98a2c6b20d1dfcff8c238e6e3f8a1
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2024-02-26 03:31:39 +0300

    Add welcome message and instructions for scanning

--- app/page.js
+++ app/page.js
@@ -2,6 +2,9 @@ export default function Home() {
   return (
     <div className="flex flex-col">
       <h1 className="text-4xl font-bold">Home</h1>
+      <p className="text-xl">Welcome to DL Scanner</p>
+      <p>New scan?</p>
+      <p>Click on the capture image tab to start scanning</p>
     </div>
-  )
+  );
 }
