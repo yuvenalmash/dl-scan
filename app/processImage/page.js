8292166ab5096e370d4e74f9354669d291848dc3
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2024-02-26 03:31:29 +0300

    Add processImage page component

---
+++ app/processImage/page.js
@@ -0,0 +1,8 @@
+export default function processImage() {
+  return (
+    <div className="flex flex-col">
+      <h1 className="text-4xl font-bold">Process Image</h1>
+      <p className="text-xl">Processing image</p>
+    </div>
+  );
+}
