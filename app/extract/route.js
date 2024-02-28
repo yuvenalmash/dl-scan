2e76ef277d699680a2c789b53556e560890724af
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2024-02-28 14:57:44 +0300

    Refactor route.js to use double quotes

--- app/extract/route.js
+++ app/extract/route.js
@@ -1,19 +1,22 @@
-import { exec } from 'child_process';
-import { NextResponse } from 'next/server';
+import { exec } from "child_process";
+import { NextResponse } from "next/server";
 
 export async function POST(request) {
   const { image } = await request.json();
 
   const resultPromise = new Promise((resolve, reject) => {
-    exec(`source venv/bin/activate && python3 extract.py ${image}`, (error, stdout, stderr) => {
-      if (error) {
-        console.error('Error extracting text:', error);
-        reject(error);
+    exec(
+      `source venv/bin/activate && python3 utils/extract.py ${image}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error("Error extracting text:", error);
+          reject(error);
+        }
+        console.log("stdout:", stdout);
+        console.log("stderr:", stderr);
+        resolve(stdout);
       }
-      console.log('stdout:', stdout);
-      console.log('stderr:', stderr);
-      resolve(stdout);
-    })
+    );
   });
   const result = await resultPromise;
 
