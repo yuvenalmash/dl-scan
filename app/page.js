6156407fc280675efda3cdd2daecd3886e68b150
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2024-02-29 05:47:11 +0300

    Update page.js with improved text formatting

--- app/page.js
+++ app/page.js
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-full">
-      <h1 className="text-4xl font-bold text-center">Welcome to DL Scanner</h1>
-      <p className="text-lg text-center mt-4 mb-8">
-        Start scanning driving license with ease
+      <h1 className="text-3xl sm:text-4xl font-bold text-center">Welcome to DL Scanner</h1>
+      <p className="text-md sm:text-lg text-center mt-4 mb-8">
+        Start scanning driving licenses with ease
       </p>
       <Link href="/captureImage" className="btn shadow-md shadow-lime-500">
         Capture Image
